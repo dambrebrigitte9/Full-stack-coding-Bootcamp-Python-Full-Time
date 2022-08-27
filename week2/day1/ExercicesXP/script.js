b0b4b90c17feb33a7fce3c36052c0d4b111cff92
dambrebrigitte9 <dambrebrigitte9@gmail.com>
2022-08-27 23:39:40 +0200

    insertion se mes fichiers du jours 1 en javscript

---
+++ week2/day1/ExercicesXP/script.js
@@ -0,0 +1,157 @@
+//exercie 1
+let monplats=poisson;
+let repasPrefere=diner;
+console.log('I eat ' +monplats+' '+ 'at every ' + repasPrefere+' ');
+
+
+//exercice2
+let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+let x=myWatchedSeries.length;
+console.log(x);
+
+let serie='and the bang theory';
+console.log('I watched 3 series : ' +myWatchedSeries);
+
+myWatchedSeries[2]='amis';//modifier le nom d'une variable en utilisant l'index 
+myWatchedSeries.push='gameOfTrone';//ajouter une variable  a la fin d'un tabaux
+myWatchedSeries.unshift='365 day';//ajouter une variable au debut d'un tablaux
+myWatchedSeries.splice(1,1,0);// supprimer le deuxieme element du tablaux
+myWatchedSeries[1];
+let str='money heist';//permet
+str.substring(2,3);//d'aficher la troisieme lettre de  « money heist ».
+console.log(myWatchedSeries);
+//exercice3
+const tempera='0° C';
+let temperaEnFahrenheit= +tempera /5*9+32
+//https://www.tutorialspoint.com/temperature-converter-using-javascript#
+
+//exercice4
+console.log(a+b) //first expression
+    //PREDICTION:the outcome of a+b  in the first expression will be 55 because 34 and 21 are the number;
+    //ACTUAL:55
+console.log(a+b) //second expression
+//PREDICTION:the outcome of a+b in thesecond expression will be 23 because we are change his value by 2;
+//ACTUAL:23;
+//3.  values of c is undefined because it have not a value;
+console.log(3 + 4 + '5');
+//ACTUAL: the outcome is 75;
+
+
+//exercie 5
+typeof(15)
+// Prediction: ceci est un nombre
+// Actual:'number'
+typeof(5.5)
+// Prediction: ceci est un nombre
+// Actual: 'number'
+typeof(NaN)
+// Prediction:  ceci n'est  pas un nombre
+// Actual: 'number'
+typeof("hello")
+// Prediction: ceci est un mot
+// Actual:'string'
+typeof(true)
+// Prediction: ceci est un boolean
+// Actual:' boolean'
+typeof(1 != 2)
+// Prediction:eci est un boolean
+// Actual: 'boolean'
+"hamburger" + "s"
+// Prediction: une concatenation de hamburger et s 
+// Actual:'hamburgers'
+"hamburgers" - "s"
+// Prediction: ceci n'est pas nombre
+// Actual:NaN
+"1" + "3"
+// Prediction: il s'agit d'une concatenation de 1 et 3 qui va doner 13 
+// Actual:'13'
+"1" - "3"
+// Prediction: concatenation de 1 et -3 qui va donner -2
+// Actual: -2
+"johnny" + 5
+// Prediction: concatenation de johnny et 5 qui va donner johnny5
+// Actual:'johnny5'
+"johnny" - 5
+// Prediction: ceci n'est pas un nombre
+// Actual:NaN
+99 * "hello"
+// Prediction: ceci n'est pas un nombre
+// Actual:NaN
+1 != 1
+// Prediction: 1 n'est pas different de 1
+// Actual:false
+1 == "1"
+// Prediction: 1 est egale a '1'
+// Actual:true
+1 === "1"
+// Prediction: 1 ne pet pas etre affecté a '1'
+// Actual:false
+
+
+
+
+//Exercice 6
+5 + "34"
+// Prediction: concatenation d'un nombre qui est 5 avec une chaine de caractere qui est 34 va donne 534
+// Actual:534 qui va donner 
+
+5 - "4"
+// Prediction: une soustraction d'une chaine de caractere 4 avec  un nombre  5 qui va donner -1
+// Actual:54
+
+10 % 5
+// Prediction: le modulo est 0
+// Actual:0
+
+5 % 10
+// Prediction: le modulo est 5
+// Actual:5
+
+"Java" + "Script"
+// Prediction: une concatenation de deux chaines de caractere
+// Actual: 'javaScript'
+
+" " + " "
+// Prediction: une concatenation de deux espaces vide
+// Actual:'  '
+
+" " + 0
+// Prediction: une chaine de caractere  vide et un nombre qui donne ce nombre une concatenation
+// Actual:' 0'
+
+true + true
+// Prediction:  on aditionne 1 et 1
+// Actual:2
+
+true + false 
+// Prediction: on aditionne 1 et o qui donne 1
+// Actual:1
+
+false + true
+// Prediction:  on aditionne 0 et 1 qui donne 1
+// Actual:1
+
+false - true  
+// Prediction: la soustraction de 0 et 1 qui donne -1
+// Actual:-1
+
+!true
+// Prediction: ce qui n'est pas vrai est faux
+// Actual: false
+
+3 - 4
+// Prediction: on fait le calcul de 3 et -4 qui donne -1
+// Actual:-1
+
+"Bob" - "bill"
+// Prediction: ceci n'est pas un nombre
+// Actual:NaN
+
+
+
+
+
+
+
+
+
