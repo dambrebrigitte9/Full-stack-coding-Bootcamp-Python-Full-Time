aacb28fd40b1733e84e21438470d67e21a39525b
dambrebrigitte9 <dambrebrigitte9@gmail.com>
2022-09-11 01:31:35 +0200

    suppresin du double de l'exercice 2

---
+++ week4/day1/exo XP_Gold/exercice.py
@@ -0,0 +1,19 @@
+#Exercice 1
+print(("hello world \n")*4, ("i love python \n")*4)
+
+  #exo2
+users= int(input("entre un numbre entre 1 et 12 :"))
+if (users>=3 and users<=5):
+    print("printemps")
+elif(users>=6 and users<=8):
+    print("été")
+elif(users>=9 and users<=11):
+    print("automne")
+elif(users in [12,2] ):
+    print("hiver")
+
+
+
+
+
+
