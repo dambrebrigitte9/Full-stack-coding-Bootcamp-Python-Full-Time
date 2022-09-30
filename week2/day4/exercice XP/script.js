52056346ed4b04c921b060d967d3a38968cddf7c
dambrebrigitte9 <dambrebrigitte9@gmail.com>
2022-09-30 20:04:42 +0200

    exo XP du jour 4

---
+++ week2/day4/exercice XP/script.js
@@ -0,0 +1,23 @@
+// Exercice 1 : Information
+// Instructions
+// Partie I : fonction sans paramètres
+
+// Créez une fonction appelée qui ne prend aucun paramètre.infoAboutMe()
+// La fonction doit consoler.log une phrase sur vous (c’est-à-dire votre nom, votre âge, vos passe-temps, etc.).
+// Appelez la fonction.
+
+
+// Partie II : fonction à trois paramètres
+
+// Créez une fonction appelée qui prend 3 paramètres.infoAboutPerson(personName, personAge, personFavoriteColor)
+// La fonction doit consoler.log une phrase sur la personne (c’est-à-dire « Votre nom est ..., vous êtes .. ans, votre couleur préférée est ... »)
+// Appelez la fonction deux fois avec les arguments suivants :
+// infoAboutPerson("David", 45, "blue")
+// infoAboutPerson("Josh", 12, "yellow")
+function infoAboutMe(){
+    console.log("je m'appelle Ali, j'ai 25 ans et j'aime etudier")
+}
+
+infoAboutMe()
+
+function infoAboutPerson(personName, personAge, personFavoriteColor)
