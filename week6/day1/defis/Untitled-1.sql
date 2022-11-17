7ee97c6c837b38d3ab3a8e16e3c9f5087e8afee1
unknown <dambrebrigitte9>
2022-11-17 16:46:41 +0000

    ajout

---
+++ week6/day1/defis/Untitled-1.sql
@@ -0,0 +1,7 @@
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Ouedraopgo','orelie','04/06/1895', 2);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES(NULL, NULL,'04/06/1895', 2);
+
+-- on remarque que  l'enregistremnt va echouer car les champsne doivent pas avoir un champ nule
