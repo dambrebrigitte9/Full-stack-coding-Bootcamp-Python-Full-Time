a095a5cde9f9debc6857faf37d5a6c7a38ce70da
unknown <dambrebrigitte9>
2022-12-03 15:23:48 +0000

    ajout

---
+++ week8/day2/defis/animal-info/Info/animals/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class animals(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    legs = models.CharField(max_length=30)
+    family = models.CharField(max_length=30)
+    weight = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
--- week8/day2/exercice XP/animal-info/Info/animals/models.py
+++ week8/day2/exercice XP/animal-info/Info/animals/models.py
@@ -10,3 +10,13 @@ class animals(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+class person(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30, unique=True)
+    phone_number = models.Field(max_length=30)
+    address= models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
